a33887dc1a75c2ff69b8773272163d58eb04f953
Hui <18268883295@163.com>
2019-07-28 20:14:13 +0800

    feat:bobo

---
+++ precache-manifest.6b87a8dbaa6768b647f12c1da359070e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d302adacebaa206a4f5858184a5e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7fc73f37f14689b9319",
+    "url": "/static/css/2.ced01483.chunk.css"
+  },
+  {
+    "revision": "d7fc73f37f14689b9319",
+    "url": "/static/js/2.fb22459d.chunk.js"
+  },
+  {
+    "revision": "df3e287b887920747458",
+    "url": "/static/js/main.1f30b03e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "947c31a987ddce00db5d5f41f24dd745",
+    "url": "/static/media/user-bg.947c31a9.jpg"
+  }
+]);
